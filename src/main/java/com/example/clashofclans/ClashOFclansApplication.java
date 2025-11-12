5d974bdc251e167a3f750021b2a997c8558281f0
konr4d <k.markiewicz29@proton.me>
2025-11-12 20:03:49 +0100

    init

---
+++ src/main/java/com/example/clashofclans/ClashOFclansApplication.java
@@ -0,0 +1,13 @@
+package com.example.clashofclans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashOFclansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClashOFclansApplication.class, args);
+    }
+
+}
